278431990d6a3872cc299b1a502d798f53100193
Calvin Koch <calvin.koch@uzh.ch>
2025-02-23 15:50:41 +0100

    reorganize

---
+++ server/bin/generated-sources/annotations/ch/uzh/ifi/hase/soprafs24/rest/mapper/DTOMapperImpl.java
@@ -0,0 +1,44 @@
+package ch.uzh.ifi.hase.soprafs24.rest.mapper;
+
+import ch.uzh.ifi.hase.soprafs24.entity.User;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.UserGetDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.UserPostDTO;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-02-23T15:50:31+0100",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.41.0.z20250115-2156, environment: Java 21.0.5 (Eclipse Adoptium)"
+)
+public class DTOMapperImpl implements DTOMapper {
+
+    @Override
+    public User convertUserPostDTOtoEntity(UserPostDTO userPostDTO) {
+        if ( userPostDTO == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setName( userPostDTO.getName() );
+        user.setUsername( userPostDTO.getUsername() );
+
+        return user;
+    }
+
+    @Override
+    public UserGetDTO convertEntityToUserGetDTO(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserGetDTO userGetDTO = new UserGetDTO();
+
+        userGetDTO.setId( user.getId() );
+        userGetDTO.setName( user.getName() );
+        userGetDTO.setUsername( user.getUsername() );
+        userGetDTO.setStatus( user.getStatus() );
+
+        return userGetDTO;
+    }
+}
